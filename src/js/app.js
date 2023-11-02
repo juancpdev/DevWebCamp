75a007070762e876d436032630ff1264641838a4
Juan Cruz <juan_pineda_12@hotmail.es>
2023-11-02 14:31:12 -0300

    uptadeModal

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,6 @@ function autocompletadoFormulario() {
     // Selecciona todos los inputs
     const inputs = document.querySelectorAll('.formulario__input');
 
-
     inputs.forEach(input => {
         input.addEventListener('input', function() {
             const label = this.nextElementSibling; // Suponiendo que el label siempre viene después del input
@@ -33,16 +32,14 @@ function modalAuth() {
     const btnLogin = document.querySelectorAll(".login");
     const btnRegistro = document.querySelectorAll(".registro");
     const btnOlvide = document.querySelectorAll(".olvide");
-
-    const cerrarModalBtns = document.querySelectorAll(".modal__close");
+    const btnCerrar = document.querySelectorAll(".modal__close");
 
     btnLogin.forEach(btn => {
         btn.addEventListener("click", function(e) {
             e.preventDefault();
 
             // Agrega esta línea para restablecer el formulario dentro del otro modal
-            modalRegistro.querySelector("form").reset();
-            modalOlvide.querySelector("form").reset();
+            reiniciarFormu(modalRegistro, modalOlvide);
             modalAnimacion(modalLogin, modalRegistro, modalOlvide);
         });
     });
@@ -52,8 +49,7 @@ function modalAuth() {
             e.preventDefault();
 
             // Agrega esta línea para restablecer el formulario dentro del otro modal
-            modalLogin.querySelector("form").reset();
-            modalOlvide.querySelector("form").reset();
+            reiniciarFormu(modalLogin, modalOlvide);
             modalAnimacion(modalRegistro, modalLogin, modalOlvide);
         });
     });
@@ -63,17 +59,15 @@ function modalAuth() {
             e.preventDefault();
 
             // Agrega esta línea para restablecer el formulario dentro del otro modal
-            modalLogin.querySelector("form").reset();
-            modalRegistro.querySelector("form").reset();
+            reiniciarFormu(modalLogin, modalRegistro);
             modalAnimacion(modalOlvide, modalRegistro, modalLogin);
         });
     });
 
-    cerrarModalBtns.forEach(btn => {
+    btnCerrar.forEach(btn => {
         btn.addEventListener("click", function() {
+            cerrarModal();
             const modal = this.closest(".modal");
-            // Cambia la opacidad a 0 con una transición
-            modal.style.opacity = "0";
             setTimeout(() => {
                 modal.style.display = "none";
                 document.body.style.overflow = "auto";
@@ -85,26 +79,48 @@ function modalAuth() {
     window.addEventListener("click", function(event) {
         fondoNegro.forEach(fondo => {
             if (event.target === fondo) {
-                event.target.parentElement.style = "none";
-                document.body.style.overflow = "auto"; // Permite el desplazamiento de nuevo
+                cerrarModal();
+
+                setTimeout(() => {
+                    const modales = document.querySelectorAll(".modal");
+                    modales.forEach(modal => {
+                        modal.style.display = "none";
+                        document.body.style.overflow = "auto"; // Permite el desplazamiento de nuevo
+                        modal.querySelector("form").reset();
+                    })
+                }, 200);
             }
         })
     });
 }
 
-function noScroll() {
-    document.body.style.overflow = "hidden"; // Evita el desplazamiento
+function reiniciarFormu(modal1, modal2) {
+    // Agrega esta línea para restablecer el formulario dentro del otro modal
+    modal1.querySelector("form").reset();
+    modal2.querySelector("form").reset();
 }
 
 function modalAnimacion(modal, modal2, modal3) {
     modal.style.display = "block";
     modal2.style.display = "none";
     modal3.style.display = "none";
-    
-    // Cambia la opacidad a 1 con una transición
-    setTimeout(() => {
-        modal.style.opacity = "1";
-    }, 0);
 
-    noScroll();
+    document.body.style.overflow = "hidden"; // Evita el desplazamiento
+
+    setTimeout(() => {
+        const auth = document.querySelectorAll(".auth__modal");
+        auth.forEach(formu => {
+            formu.classList.add("animar");
+        })
+    }, 0);
+}
+
+
+function cerrarModal() {
+    setTimeout(() => {
+        const auth = document.querySelectorAll(".auth__modal");
+        auth.forEach(formu => {
+            formu.classList.remove("animar");
+        })
+    }, 0);
 }
